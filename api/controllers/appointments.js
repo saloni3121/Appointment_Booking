e33fd34b91bfacb5732207dd434788135bb02b09
saloni <saloni.patel01@gmail.com>
2022-05-09 12:11:15 +0530

    clash avoided

---
+++ api/controllers/appointments.js
@@ -0,0 +1,62 @@
+var Model = require('../models/index.js');
+const { Appointment, Slot } = Model;
+const Nexmo = require("nexmo");
+
+const appointmentController = {
+  all(req, res) {
+    // Returns all appointments
+    Appointment.find({}).exec((err, appointments) => res.json(appointments));
+  },
+  create(req, res) {
+    var requestBody = req.body;
+
+    var newslot = new Slot({
+      slot_time: requestBody.slot_time,
+      slot_date: requestBody.slot_date,
+      created_at: Date.now()
+    });
+    newslot.save();
+    // Creates a new record from a submitted form
+    var newappointment = new Appointment({
+      name: requestBody.name,
+      email: requestBody.email,
+      phone: requestBody.phone,
+      slots: newslot._id
+    });
+
+    const nexmo = new Nexmo({
+      apiKey: "00fee5ba",
+      apiSecret: "mgpDrZEipp7nkoAr"
+    });
+
+    let msg =
+      requestBody.name +
+      " " +
+      "this message is to confirm your appointment at" +
+      " " +
+      requestBody.appointment;
+
+    // and saves the record to
+    // the data base
+    newappointment.save((err, saved) => {
+      // Returns the saved appointment
+      // after a successful save
+      Appointment.find({ _id: saved._id })
+        .populate("slots")
+        .exec((err, appointment) => res.json(appointment));
+
+      const from = "7972911310";
+      const to = "7972911310";
+
+      nexmo.message.sendSms(from, to, msg, (err, responseData) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.dir(responseData);
+        }
+      });
+    });
+  }
+};
+
+module.exports = appointmentController;
